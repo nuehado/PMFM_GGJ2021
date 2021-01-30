1a2bb259e177d860282a390dceac606e315b23fc
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2021-01-30 11:51:47 -0800

    starting new fires et al

--- Assets/Scripts/Kindling.cs
+++ Assets/Scripts/Kindling.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Kindling : Interactable_Source
 {
     public Kindling_Properties properties;
+
+    public override bool IsEquippable()
+    {
+        return true;
+    }
 }
