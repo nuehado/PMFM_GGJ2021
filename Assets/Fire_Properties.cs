14932022d06c58182a9c5645e4217c8c21afebc8
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2021-01-29 21:02:40 -0800

    Fire

---
+++ Assets/Fire_Properties.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Fire_Properties : ScriptableObject
+{
+    public float startingFuel;
+    public float fuelUsageRate; //fuel used per second
+}
