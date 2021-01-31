650ffd810dcee92ac5f996fa9d6774e8fc558065
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2021-01-31 09:11:18 -0800

    first boat not at start beach

--- Assets/Scripts/BoatManager.cs
+++ Assets/Scripts/BoatManager.cs
@@ -22,6 +22,8 @@ public class BoatManager : MonoBehaviour
     private Image uiSW;
     private Image uiSE;
 
+    int boatsSummoned = 0;
+
     private void Start()
     {
         StartCoroutine(ManageBoats());
@@ -104,7 +106,16 @@ public class BoatManager : MonoBehaviour
     private IslandEdge PickRandomEdge()
     {
         int i = Random.Range(0, 4);
-        return (IslandEdge)i;
+        IslandEdge proposedEdge = (IslandEdge)i;
+        if (boatsSummoned > 0 || proposedEdge != IslandEdge.SE)
+        {
+            boatsSummoned++;
+            return proposedEdge;
+        }
+        else
+        {
+            return PickRandomEdge();
+        }
     }
 
     private void SetUpBoat(IslandEdge edge)
